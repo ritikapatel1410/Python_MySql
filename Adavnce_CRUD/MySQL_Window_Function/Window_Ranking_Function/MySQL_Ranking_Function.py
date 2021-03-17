e749eb5c511efc1875a4d270eff91bf9c5701106
patelritika1410 <patelrit1410@gmail.com>
2021-03-18 00:11:44 +0530

    Refactor : new dir MySQL_Window_Function/Window_Ranking_Function added

---
+++ Adavnce_CRUD/MySQL_Window_Function/Window_Ranking_Function/MySQL_Ranking_Function.py
@@ -0,0 +1,119 @@
+'''
+@Author: Ritika Patidar
+@Date: 2021-03-17 23:10:10
+@Last Modified by: Ritika Patidar
+@Last Modified time: 2021-03-17 23:10:38  
+@Title : perform window ranking function on table
+'''
+import mysql.connector as mysql
+import os
+import sys
+sys.path.insert(0,os.path.realpath("LogFile"))
+import loggerfile
+from decouple import config
+
+class Ranking_Function:
+    """
+        Description:
+            this class is define for show the functionality of window Ranking Function
+    """
+    def __init__(self):
+        self.mydb=mysql.connect(host=config('DB_HOST'),user=config('DB_USERNAME'),password=config('DB_PASSWORD'),database=config('DB_DATABASE'))
+        self.mycursor = self.mydb.cursor()
+    def RANK_Fuction(self):
+        """
+            Description:
+                this function is define for RANK sale of sales table
+            Parameter:
+                None
+            Return:
+                None
+        """
+        try:
+            print("================================ RANK of sale ===================================")
+            self.mycursor.execute("SELECT Year, Product, Sale, RANK() OVER(PARTITION BY product order by Sale desc) AS Rank_Sale FROM Sales")
+            rank = self.mycursor.fetchall()
+            for records in rank:
+                print(records)
+            loggerfile.Logger("info","RANK {0} successfully".format( count))
+        except Exception as error:
+            loggerfile.Logger("error","{0} occured".format(error))
+
+    def DENSE_RANK_Fuction(self):
+        """
+            Description:
+                this function is define for DENSE_RANK sale of sales table
+            Parameter:
+                None
+            Return:
+                None
+        """
+        try:
+            print("================================ sum of Sale ===================================")
+            self.mycursor.execute("SELECT Year, Product, Sale, DENSE_RANK() OVER(PARTITION BY product order by Sale desc) AS Dense_Rank_Sale FROM Sales")
+            dense_rank = self.mycursor.fetchall()
+            for records in dense_rank:
+                print(records)
+            loggerfile.Logger("info","DENSE_RANK {0} get successfully".format(Sum))
+        except Exception as error:
+            loggerfile.Logger("error","{0} occured".format(error))
+
+
+    def PERCENT_RANK_Fuction(self):
+        """
+            Description:
+                this function is define for PERCENT_RANK of sale of sales table
+            Parameter:
+                None
+            Return:
+                None
+        """
+        try:
+            print("================================ PERCENT_RANK sale===================================")
+            self.mycursor.execute("SELECT Year, Product, Sale, PERCENT_RANK() OVER(PARTITION BY product order by Sale desc) AS Percent_Rank_sale FROM Sales")
+            percentage_rank=self.mycursor.fetchall()
+            for records in percentage_rank:
+                print(records)
+            loggerfile.Logger("info","PERCENT_RANK  {0} get successfully".format(AVG))
+        except Exception as error:
+            loggerfile.Logger("error","{0} occured".format(error))
+
+    def ROW_NUMBER_Fuction(self):
+        """
+            Description:
+                this function is define for ROW_NUMBER sale of sales table
+            Parameter:
+                None
+            Return:
+                None
+        """
+        try:
+            print("================================ ROW_NUMBER sale ===================================")
+            self.mycursor.execute("SELECT Year, Product, Sale, ROW_NUMBER() OVER(PARTITION BY product order by Sale desc) AS Row_Number_sale FROM Sales")
+            row_number=self.mycursor.fetchall()
+            for records in row_number:
+                print(records)
+            loggerfile.Logger("info","ROW_NUMBER {0} get successfully".format(Min))
+        except Exception as error:
+            loggerfile.Logger("error","{0} occured".format(error))
+
+
+    def  CUME_DIST(self):
+        """
+            Description:
+                this function is define for CUME_DIST of Sale of Sales table
+            Parameter:
+                None
+            Return:
+                None
+        """
+        try:
+            print("================================ CUME_DIST sale ===================================")
+            self.mycursor.execute("SELECT Year, Product, Sale,  CUME_DIST() OVER(PARTITION BY product order by Sale desc) AS Dume_Dist_Sale FROM Sales")
+            cume_dist=self.mycursor.fetchall()
+            for records in cume_dist:
+                print(records)
+            loggerfile.Logger("info","CUME_DIST  {0} get successfully".format(Max))
+        except Exception as error:
+            loggerfile.Logger("error","{0} occured".format(error))
+
--- Adavnce_CRUD/MySQL_Window_Function/Window_Ranking_Function/main.py
+++ Adavnce_CRUD/MySQL_Window_Function/Window_Ranking_Function/main.py
@@ -3,7 +3,7 @@
 @Date: 2021-03-15 0:10:10
 @Last Modified by: Ritika Patidar
 @Last Modified time: 2021-03-15 10:10:38  
-@Title : main code of perform window aggregate function on table
+@Title : main code of perform window ranking function on table
 """
 
 
@@ -11,35 +11,31 @@ import sys
 import os
 sys.path.insert(0,os.path.relpath("LogFile"))
 import loggerfile
-from MySQL_Aggregate_Function import Aggregate_Function
+from MySQL_Ranking_Function import Ranking_Function
 
 def main():
     """
     Description:
-        this function is define for mode of show the functionality of Aggregate Function
+        this function is define for mode of show the functionality of window Ranking Function
     Parameter:
         None
     Return:
         None
     """
     try:
-        Obj_Aggregate_Function=Aggregate_Function()
-        mode=int(input("================================= Select Mode For Aggregate Function =====================================\n0 : Count\n====================================\n1 : Sum\n==============================================\n2 : AVG\n==================================================================\n3 : Min\n=============================================\n4 : Max\n===========================================================\n5 : First Value\n================================================================\n6 : Last Value\n=====================\n7 : quit\n=====================\nenter: "))
+        Obj_Ranking_Function=Ranking_Function()
+        mode=int(input("================================= Select Mode For Ranking Function =====================================\n0 : Rank\n====================================\n1 : Dense Rank\n==============================================\n2 : Percentage Rank\n==================================================================\n3 : Row number\n=============================================\n4 : Dist Rank\n==============================================\n5 : quit\n=====================\nenter: "))
         if(mode==0):
-            Obj_Aggregate_Function.Count_Fuction()
+            Obj_Ranking_Function.RANK_Fuction()
         elif(mode==1):
-            Obj_Aggregate_Function.Sum_Fuction()
+            Obj_Ranking_Function.DENSE_RANK_Fuction()
         elif(mode==2):
-            Obj_Aggregate_Function.AVG_Fuction()
+            Obj_Ranking_Function.PERCENT_RANK_Fuction()
         elif(mode==3):
-            Obj_Aggregate_Function.Min_Fuction()
+            Obj_Ranking_Function.ROW_NUMBER_Fuction()
         elif(mode==4):
-            Obj_Aggregate_Function.Max_Fuction()
+            Obj_Ranking_Function.CUME_DIST()
         elif(mode==5):
-            print(Obj_Aggregate_Function.First_Fuction())
-        elif(mode==6):
-            print(Obj_Aggregate_Function.Last_Fuction())
-        elif(mode==7):
             sys.exit()
         loggerfile.Logger("info","succesfully select the mode")
     except Exception as error:
